741719093e42f95185ed5d906d0d5d3c68be9ea8
nadiabk <nadia.ben-kalia@efrei.net>
2025-01-05 18:16:28 +0100

    ajout conditions remplissage champs

--- Back-end/Data Access/Access_Projection.js
+++ Back-end/Data Access/Access_Projection.js
@@ -1,7 +1,18 @@
 const pool = require('../Database/config_database.js');
 
 exports.createProjection = async (projection) => {
-    const { Nom_cinema, No_salle, No_seance, Heure_debut, Heure_fin, ID_film } = seance;
+    const { Nom_cinema, No_salle, No_seance, Heure_debut, Heure_fin, ID_film } = projection;
+
+    if (!Nom_cinema || !No_salle || !No_seance || !Heure_debut || !Heure_fin || !ID_film) {
+        throw new Error('Veuillez indiquer tous les champs');
+    }
+
+    if (typeof Nom_cinema !== 'string' || typeof No_salle !== 'number' || typeof No_seance !== 'number' ||
+        typeof Heure_debut !== 'string' || typeof Heure_fin !== 'string' || typeof ID_film !== 'number')
+    {
+        throw new Error('Type de données saisies invalides');
+    }
+
     const [result] = await pool.query(
         'INSERT INTO Seance (Nom_cinema, No_salle, No_seance, Heure_debut, Heure_fin, ID_film) VALUES (?, ?, ?, ?, ?, ?)',
         [Nom_cinema, No_salle, No_seance, Heure_debut, Heure_fin, ID_film]
