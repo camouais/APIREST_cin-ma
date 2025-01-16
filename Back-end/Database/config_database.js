4bf260279664aa646c05c0ad98cd75bb2cd441b9
nadiabk <nadia.ben-kalia@efrei.net>
2025-01-16 21:48:19 +0100

    lien vers les differentes pages + ajout recherche film

--- Back-end/Database/config_database.js
+++ Back-end/Database/config_database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise'); // Use promise-based MySQL
 const pool = mysql.createPool({
     host: 'localhost',      // Hostname of your database server
     user: 'root',           // Database username
-    password: 'root',   // Database password
+    password: 'rootroot',   // Database password
     database: 'REST_API',   // Database name
     waitForConnections: true,
     connectionLimit: 10,    // Maximum number of connections in the pool
